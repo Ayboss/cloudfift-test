1c99b64f49cb2c8d7377ca3ea63e3456501f4ee4
Ayboss <bamiayo90@gmail.com>
2021-07-14 08:20:58 +0100

    link to share your story

--- src/components/MajorPerson.js
+++ src/components/MajorPerson.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import womanshopping from '../assets/womanshopping.svg'
 
 function MajorPerson() {
@@ -9,13 +10,13 @@ function MajorPerson() {
                     <h3 className="majorperson__heading">Josiah's Experience</h3>
                     <span className="majorperson__tag">VENDOR</span>
                     <p className="majorperson__p">I had the best experience shopping with vasiti. Usability of the website was great, very good customer service, an all round great experience. I would definately be coming back! I had the best experience shopping with vasiti. Usability of the website was great, very good customer service, an all round great experience. I would definately be coming back!</p>
-                        <a className="majorperson__link" href="#">
+                        <Link className="majorperson__link" to="/story">
                             SHARE YOUR OWN STORY!
                             <svg width="297" height="12" viewBox="0 0 297 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M1 11.2012C71.3333 5.86791 228.8 -3.19875 296 3.20125" stroke="#FF5C00"/>
                             </svg>
 
-                        </a>
+                        </Link>
                 </div>
                 <div className="majorperson__img">
                     <img className="majorperson__img--img" src={womanshopping} alt="majorperson" />
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 //styles in HeroTestimonial.css
 import testimonialImg from '../assets/testimonial.svg'
 
@@ -22,13 +23,12 @@ function Testimonial() {
                         very good customer service,
                         an all round great experience. 
                         I would definately be coming back!</p>
-                        <a className="testimonial__link" href="#">
+                        <Link className="testimonial__link" to="/story">
                             SHARE YOUR OWN STORY!
                             <svg width="297" height="12" viewBox="0 0 297 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M1 11.2012C71.3333 5.86791 228.8 -3.19875 296 3.20125" stroke="#FF5C00"/>
                             </svg>
-
-                        </a>
+                        </Link>
                 </div>
             </div>
         </section>
