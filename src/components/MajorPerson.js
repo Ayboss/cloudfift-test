cbc7e222b83f3724ae82a95162f98c3d97f0c229
Ayboss <bamiayo90@gmail.com>
2021-07-14 14:17:49 +0100

    space added to icons

--- src/components/MajorPerson.js
+++ src/components/MajorPerson.js
@@ -12,7 +12,7 @@ function MajorPerson() {
                     <p className="majorperson__p">I had the best experience shopping with vasiti. Usability of the website was great, very good customer service, an all round great experience. I would definately be coming back! I had the best experience shopping with vasiti. Usability of the website was great, very good customer service, an all round great experience. I would definately be coming back!</p>
                         <Link className="majorperson__link" to="/story">
                             SHARE YOUR OWN STORY!
-                            <svg width="297" height="12" viewBox="0 0 297 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg className="underline" width="297" height="12" viewBox="0 0 297 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M1 11.2012C71.3333 5.86791 228.8 -3.19875 296 3.20125" stroke="#FF5C00"/>
                             </svg>
 
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -25,7 +25,7 @@ function Testimonial() {
                         I would definately be coming back!</p>
                         <Link className="testimonial__link" to="/story">
                             SHARE YOUR OWN STORY!
-                            <svg width="297" height="12" viewBox="0 0 297 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg className="underline" width="297" height="12" viewBox="0 0 297 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M1 11.2012C71.3333 5.86791 228.8 -3.19875 296 3.20125" stroke="#FF5C00"/>
                             </svg>
                         </Link>
